408009f59296f6a7ac9ba490625a05d91c4fdcbe
JR <josera980@hotmail.com>
2023-03-17 20:56:33 -0400

    create supplier entity

---
+++ src/entities/supplier.js
@@ -0,0 +1,8 @@
+export class Supplier {
+    constructor(id, name, number, email) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+}
