e9f1d7d931a9f68942f295074b41606d62feec7d
JR <josera980@hotmail.com>
2023-04-07 12:02:03 -0400

    Creating IoC file.

---
+++ src/ioc/Ioc.js
@@ -0,0 +1,37 @@
+class Ioc {
+    constructor() {
+        this.services = {};
+    }
+
+    register(serviceName, service) {
+
+        if (!serviceName) {
+            throw new Error("Please verify service name.");
+        }
+
+        if (this.services[serviceName]) {
+            throw new Error("Service already created.");
+        }
+
+        this.services[serviceName] = service;
+        
+    }
+
+    getService(serviceName) {
+        if (!serviceName) {
+            throw new Error("Please verify service name.");
+        }
+
+        if (!this.services[serviceName]) {
+            throw new Error("Service not found.");
+        }
+
+        return this.services[serviceName];
+        
+    }
+
+}
+
+let iocInstance = Object.freeze(new Ioc());
+
+export default iocInstance;
